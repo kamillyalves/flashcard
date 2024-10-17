f7a9d40364aac447420d3f55092175098b22daa6
kamillyalves <108742566+kamillyalves@users.noreply.github.com>
2024-10-17 11:16:05 -0300

    1

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação'
+    'O que é Python'
+    'O python é uma linguagem de programação'
+)
+
+criaCartao(
+   'geografia'
+   'Qual a capital da França?'
+   'A capital da frança é Paris' 
+)
+
+criaCartao(
+    'Programação'
+    'O que é uma função?'
+    'Uma função é um bloco de codigos que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa'
+    'Como se diz oi em ingles?'
+    'Oi em inles é Hi (RAI)'
+)
